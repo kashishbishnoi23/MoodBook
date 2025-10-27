a18c70b0a2da4ae9b1e13eb06873d1b2cbd0eee8
kashish <kashishbishnoi23@gmail.com>
2025-10-27 17:50:30 +0530

    update

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.listen(port, ()=>{
     console.log("Server is running on port " , port);
 }) 
 
-app.get("/", (req, res)=>{
-    res.sendFile("index.html");
-})
+app.use(express.static(path.join(__dirname, "public")));
+
+// Optional: agar koi route na mile, to index.html bhej do
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
